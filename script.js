f2b2068465d8aa449476f0afce89758fd91b7b60
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-10 02:17:39 +0530

    Add Submit Button and JS Function

---
+++ script.js
@@ -0,0 +1,13 @@
+function firstplayername(){
+    let fpn = document.getElementById('firstplayer').value;
+    let disp1 = document.createElement('div');
+    disp1.innerHTML = fpn;
+    console.log(disp1);
+
+}
+
+// function secondplayername(){
+//     let spn = document.getElementById('secondplayer').value;
+//     console.log(spn);
+// }
+
