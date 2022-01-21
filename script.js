45211d1316ae1d9a6f553174138bccee7afb44bf
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-21 19:29:47 +0530

    Add Reset Game and many More

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+function newgame(){
+    location.reload();
+}
 function firstplayer(){
     var firstplayername = document.getElementById('firstplayer').value;
     // console.log(firstplayername);
     document.getElementById('firstplayerheading').innerHTML = firstplayername;
     document.getElementById('firstplayer').style.display = "none";
     document.getElementById('rsbtn1').style.display = "none";
+
+    play1();
 }
 
 function secondplayer(){
-    const secondplayername = document.getElementById('secondplayer').value;
+    secondplayername = document.getElementById('secondplayer').value;
     // console.log(secondplayername);
     document.getElementById('secondplayerheading').innerHTML = secondplayername;
     document.getElementById('secondplayer').style.display = "none";
     document.getElementById('lsbtn2').style.display = "none";
+
+    play2();
 }
+
+$('#playerbtn-1').click(function roll1(){
+
+});
