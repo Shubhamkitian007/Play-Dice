d0431653590655681df17c92849b4e42b6766055
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-21 16:58:31 +0530

    PlayerName Submited

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function firstplayername(){
-    let fpn = document.getElementById('firstplayer').value;
-    let disp1 = document.createElement('div');
-    disp1.innerHTML = fpn;
-    console.log(disp1);
-
+function firstplayer(){
+    var firstplayername = document.getElementById('firstplayer').value;
+    // console.log(firstplayername);
+    document.getElementById('firstplayerheading').innerHTML = firstplayername;
+    document.getElementById('firstplayer').style.display = "none";
+    document.getElementById('rsbtn1').style.display = "none";
 }
 
-// function secondplayername(){
-//     let spn = document.getElementById('secondplayer').value;
-//     console.log(spn);
-// }
-
+function secondplayer(){
+    const secondplayername = document.getElementById('secondplayer').value;
+    // console.log(secondplayername);
+    document.getElementById('secondplayerheading').innerHTML = secondplayername;
+    document.getElementById('secondplayer').style.display = "none";
+    document.getElementById('lsbtn2').style.display = "none";
+}
