44ad676f6565424110e0a83acfecacf2b6ee84bf
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-22 12:17:04 +0530

    Dice Added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var count = 0;
 function newgame(){
     location.reload();
 }
@@ -7,8 +8,9 @@ function firstplayer(){
     document.getElementById('firstplayerheading').innerHTML = firstplayername;
     document.getElementById('firstplayer').style.display = "none";
     document.getElementById('rsbtn1').style.display = "none";
+    
 
-    play1();
+    // play1();
 }
 
 function secondplayer(){
@@ -17,10 +19,33 @@ function secondplayer(){
     document.getElementById('secondplayerheading').innerHTML = secondplayername;
     document.getElementById('secondplayer').style.display = "none";
     document.getElementById('lsbtn2').style.display = "none";
-
-    play2();
+    document.getElementById('droll').style.visibility = "visible";
+    // play2();
 }
 
-$('#playerbtn-1').click(function roll1(){
+function diceroll(){
+    count++;
+    if(count == 1){
+        // console.log(count);
+        const firstRandomNum = Math.floor(Math.random() *6) + 1;
+        console.log(firstRandomNum);
+        const firstDiceImage = 'images/'+ firstRandomNum + '.png';
+        document.getElementById('firstplayerimage').setAttribute('src',firstDiceImage);
+    
+    }
+    else if(count == 2){
+        // console.log(count);
+        const secondRandomNum = Math.floor(Math.random() *6) + 1;
+        console.log(secondRandomNum);
+        const secondDiceImage = 'images/'+ secondRandomNum + '.png';
+        document.getElementById('secondplayerimage').setAttribute('src',secondDiceImage);
+        count = 0;
+    }
+}
 
-});
+
+
+
+// $('#playerbtn-1').click(function roll1(){
+
+// });
