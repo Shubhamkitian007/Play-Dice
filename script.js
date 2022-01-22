ef0aa0bc27df611f1b08747d73b1fa97b9eeb916
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-22 23:33:43 +0530

    Add Dice Image

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ function firstplayer(){
     document.getElementById('firstplayerheading').innerHTML = firstplayername;
     document.getElementById('firstplayer').style.display = "none";
     document.getElementById('rsbtn1').style.display = "none";
+    document.getElementById('droll').style.display = "none";
     
 
     // play1();
 }
 
 function secondplayer(){
-    secondplayername = document.getElementById('secondplayer').value;
+    this.secondplayername = document.getElementById('secondplayer').value;
     // console.log(secondplayername);
     document.getElementById('secondplayerheading').innerHTML = secondplayername;
     document.getElementById('secondplayer').style.display = "none";
     document.getElementById('lsbtn2').style.display = "none";
-    document.getElementById('droll').style.visibility = "visible";
+    document.getElementById('droll').style.display = "block";
     // play2();
 }
 
@@ -27,25 +28,20 @@ function diceroll(){
     count++;
     if(count == 1){
         // console.log(count);
-        const firstRandomNum = Math.floor(Math.random() *6) + 1;
-        console.log(firstRandomNum);
+        this.firstRandomNum = Math.floor(Math.random() *6) + 1;
         const firstDiceImage = 'images/'+ firstRandomNum + '.png';
         document.getElementById('firstplayerimage').setAttribute('src',firstDiceImage);
+        firstplayer.push(firstRandomNum);
     
     }
     else if(count == 2){
         // console.log(count);
-        const secondRandomNum = Math.floor(Math.random() *6) + 1;
-        console.log(secondRandomNum);
+        this.secondRandomNum = Math.floor(Math.random() *6) + 1;
         const secondDiceImage = 'images/'+ secondRandomNum + '.png';
         document.getElementById('secondplayerimage').setAttribute('src',secondDiceImage);
         count = 0;
     }
+
+    
 }
 
-
-
-
-// $('#playerbtn-1').click(function roll1(){
-
-// });
