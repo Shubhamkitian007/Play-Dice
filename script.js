0deb5ff1699ecb1959f86bd9526fce1faa90a619
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-01-23 17:41:17 +0530

    Add Discription

--- script.js
+++ script.js
@@ -2,46 +2,42 @@ var count = 0;
 function newgame(){
     location.reload();
 }
+
+(function(){
+    document.getElementById('droll').style.display = "none";
+})();
 function firstplayer(){
-    var firstplayername = document.getElementById('firstplayer').value;
-    // console.log(firstplayername);
+    const firstplayername = document.getElementById('firstplayer').value;
     document.getElementById('firstplayerheading').innerHTML = firstplayername;
     document.getElementById('firstplayer').style.display = "none";
     document.getElementById('rsbtn1').style.display = "none";
-    document.getElementById('droll').style.display = "none";
-    
-
-    // play1();
 }
 
 function secondplayer(){
-    this.secondplayername = document.getElementById('secondplayer').value;
-    // console.log(secondplayername);
+    const secondplayername = document.getElementById('secondplayer').value;
     document.getElementById('secondplayerheading').innerHTML = secondplayername;
     document.getElementById('secondplayer').style.display = "none";
     document.getElementById('lsbtn2').style.display = "none";
     document.getElementById('droll').style.display = "block";
-    // play2();
+    document.getElementById('displayrule').style.display = "none";
 }
 
 function diceroll(){
     count++;
     if(count == 1){
-        // console.log(count);
         this.firstRandomNum = Math.floor(Math.random() *6) + 1;
         const firstDiceImage = 'images/'+ firstRandomNum + '.png';
         document.getElementById('firstplayerimage').setAttribute('src',firstDiceImage);
-        firstplayer.push(firstRandomNum);
-    
     }
     else if(count == 2){
-        // console.log(count);
         this.secondRandomNum = Math.floor(Math.random() *6) + 1;
         const secondDiceImage = 'images/'+ secondRandomNum + '.png';
         document.getElementById('secondplayerimage').setAttribute('src',secondDiceImage);
         count = 0;
     }
-
-    
 }
 
+var firstvalue = document.getElementById('firstplayerimage').value;
+console.log(firstvalue);
+var secondtvalue = document.getElementById('secondplayerimage').value;
+console.log(secondtvalue);
