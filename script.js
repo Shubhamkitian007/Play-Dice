9c40a559d48aca75eef0f31f6d0f3cf15d80fc90
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-02-03 14:27:41 +0530

    Complete

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ function newgame(){
 
 (function(){
     document.getElementById('droll').style.display = "none";
+    document.getElementById('result').style.display = "none";
+    document.getElementById('new').style.display = "none";
 })();
+
 function firstplayer(){
     const firstplayername = document.getElementById('firstplayer').value;
     document.getElementById('firstplayerheading').innerHTML = firstplayername;
@@ -25,19 +28,50 @@ function secondplayer(){
 function diceroll(){
     count++;
     if(count == 1){
-        this.firstRandomNum = Math.floor(Math.random() *6) + 1;
+        firstRandomNum = Math.floor(Math.random() *6) + 1;
         const firstDiceImage = 'images/'+ firstRandomNum + '.png';
         document.getElementById('firstplayerimage').setAttribute('src',firstDiceImage);
+        document.getElementsByClassName('dicevalueone').value = firstRandomNum;        
+        // console.log(dicevalueone);
     }
     else if(count == 2){
-        this.secondRandomNum = Math.floor(Math.random() *6) + 1;
+        secondRandomNum = Math.floor(Math.random() *6) + 1;
         const secondDiceImage = 'images/'+ secondRandomNum + '.png';
         document.getElementById('secondplayerimage').setAttribute('src',secondDiceImage);
+        document.getElementsByClassName('dicevaluetwo').value = secondRandomNum;
+        document.getElementById('droll').style.display = "none";
+        document.getElementById('result').style.display = "block";
+        // console.log(secondRandomNum);
         count = 0;
     }
 }
 
-var firstvalue = document.getElementById('firstplayerimage').value;
-console.log(firstvalue);
-var secondtvalue = document.getElementById('secondplayerimage').value;
-console.log(secondtvalue);
+function result(){
+    firstdicevalue = document.getElementsByClassName('dicevalueone').value;
+    seconddicevalue = document.getElementsByClassName('dicevaluetwo').value;
+    fname = document.getElementById('firstplayerheading').innerHTML;
+    sname = document.getElementById('secondplayerheading').innerHTML;
+
+    if(firstdicevalue > seconddicevalue){
+        document.getElementById('box').style.display = "none";
+        document.getElementById('result').style.display = "none";
+        document.getElementById('new').style.display = "block";
+        document.getElementById('win').innerHTML = `Congratulations for Win <br> ${fname} `;
+        console.log(fname);
+    }
+    else if(seconddicevalue > firstdicevalue){
+        document.getElementById('box').style.display = "none";
+        document.getElementById('result').style.display = "none";
+        document.getElementById('new').style.display = "block";
+        document.getElementById('win').innerHTML = `Congratulations For Win <br> ${sname} `;
+        console.log(sname);
+    }
+    else{
+        document.getElementById('box').style.display = "none";
+        document.getElementById('result').style.display = "none";
+        document.getElementById('new').style.display = "block";
+        document.getElementById('win').innerHTML = `${fname} ${Sname} It's Drew`;
+        
+    }
+    // diceroll();
+}
